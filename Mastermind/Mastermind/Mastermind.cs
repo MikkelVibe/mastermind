feb6120e86d818263044bfbf2aea90ef4f292f03
Magn7559 <74953315+Magn7559@users.noreply.github.com>
2021-04-15 14:02:12 +0200

    Add files via upload

---
+++ Mastermind/Mastermind/Mastermind.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mastermind
+{
+    class Mastermind
+    {
+        public void Run()
+        {
+
+
+            Console.WriteLine("mastermind");
+
+            string line = Console.ReadLine();
+            if (line.ToUpper() == "Q")
+                return;
+
+        }
+    }
+}
---
+++ Mastermind/Mastermind/walktrough.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mastermind
+{
+    class Walktrough
+    {
+        public void Run()
+        {
+            Console.WriteLine("Mastermind");
+
+
+            string line = Console.ReadLine();
+            if (line.ToUpper() == "Q")
+                return;
+        }
+    }
+}
