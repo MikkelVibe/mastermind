05e00abd4585511e137598a984b9b453553dd0fd
MikkelVibe <mikvclangaa@hotmail.com>
2021-04-29 14:30:12 +0200

    Update to interface

--- Mastermind/Mastermind/Program.cs
+++ Mastermind/Mastermind/Program.cs
@@ -8,19 +8,60 @@ namespace Mastermind
     {
         static void Main(string[] args)
         {
-            int difficult = 1;
+            
+
+            int difficult = 0;
             bool isInt = false;
             Console.Clear(); // grunden til jeg clear er fordi hvis brugeren går tilbage fra en af spillende skal hovedmenuen vere clear 
             while (!isInt)
             {
-
                 Console.WriteLine("For fast walktrough press 1, Press 2 for to read the rules, Press 3 to begin playing Mastermind!, PRESS Q TO CLOSE PROGRAM");
                 try
                 {
-                    difficult = int.Parse(Console.ReadLine());
-                    if (difficult != 1 && difficult != 2 && difficult != 3)
+                    int line;
+                    line = int.Parse(Console.ReadLine());
+                    if (line != 1 && line != 2 && line != 3)
                         break;
                     isInt = true;
+
+                    switch (line)
+                    {
+                        case 0:
+                            return;
+
+                        case 1:
+                            Walktrough walktrough = new Walktrough(); //her refererer jeg til min klasse walktrough og navngiver den walktrough 
+                            walktrough.Run(); // her siger jeg at hvis inputtet er 1 skal den åbne klassen walktrough
+                            break;
+
+                        case 2:
+                            Rules rules = new Rules(); //her referer jeg til min klasse rules og navngiver den Rules
+                            rules.Run(); // Her siger jeg at den skal starte Rules klassen 
+                            break;
+
+                        case 3:
+                            bool issInt = false;
+                            Mastermind mastermind = new Mastermind(); //her referer jeg til min klasse mastermind og navngiver den Mastermind
+                            while (!issInt)
+                            {
+
+                                Console.WriteLine("For medium press 1, for hardmode press 2");
+                                try
+                                {
+                                    difficult = int.Parse(Console.ReadLine());
+                                    if (difficult != 1 && difficult != 2)
+                                        break;
+                                    issInt = true;
+                                    Console.Clear();
+                                }
+                                catch
+                                {
+                                    Console.Clear();
+                                }
+                            }
+                            mastermind.Run(difficult);
+                            break;
+                    }
                 }
                 catch
                 {
@@ -28,54 +69,11 @@ namespace Mastermind
                     
                 }
             }
+
+
+
             
-
-            string line = Console.ReadLine();
-            if (int.TryParse(line, out var valg)) //Checker om det er et heltal og hvis det er 1,2 eller 3
-            {
-                switch (valg)
-                {
-                    case 1:
-                        Walktrough walktrough = new Walktrough(); //her refererer jeg til min klasse walktrough og navngiver den walktrough 
-                        walktrough.Run(); // her siger jeg at hvis inputtet er 1 skal den åbne klassen walktrough
-                        break;
-
-                    case 2:
-                        Rules rules = new Rules(); //her referer jeg til min klasse rules og navngiver den Rules
-                        rules.Run(); // Her siger jeg at den skal starte Rules klassen 
-                        break;
-
-                    case 3:
-                        Mastermind mastermind = new Mastermind(); //her referer jeg til min klasse mastermind og navngiver den Mastermind
-                        int difficult = 1;
-                        bool isInt = false;
-                        while (!isInt)
-                        {
-                           
-                            Console.WriteLine("For medium press 1, for hardmode press 2");
-                            try
-                            {
-                                difficult = int.Parse(Console.ReadLine());
-                                if (difficult != 1 && difficult != 2)
-                                    break;
-                                isInt = true;
-                            }
-                            catch
-                            {
-                                Console.WriteLine("That's not 1 or 2");
-                            }
-                        }
-                        mastermind.Run(difficult); // Her siger jeg at den skal starte Rules klassen 
-                        break;
-
-                    default: // Hvis brugeren ikke har trykket hverken 1,2 eller 3 vil den bruge denne
-                        Console.WriteLine($"You can only choose between 1, 2 og 3 OR PRESS Q TO CLOSE THE PROGRAM you have chossen: {valg}. press enter to continue."); //Her skriver jeg til brugeren at inputtet var forkert og hvad de har skrevet 
-
-                        Console.ReadLine();
-                        break; //her giver jeg brugeren mulighed for at læse hvad der er sket og hvordan de kan fortsætte  
-                }
-            }
-            if (line.ToUpper() == "Q") return;
+            
         }
     }
 }
